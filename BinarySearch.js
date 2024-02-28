ebb8a5e2313daafe214e0cebd34a9102c7d17139
anshumaanbaaghi <ganumumbai067@gmail.com>
2024-02-28 21:56:12 +0530

    LowerBoundInLogn

--- BinarySearch.js
+++ BinarySearch.js
@@ -1,16 +1,35 @@
 const binarySearch = (arr, k) => {
-    let left = 0;
-    let right = arr.length - 1;
-    let ans = 0;
-    while (left < right) {
-        const mid = Math.floor((right + left) / 2);
-        if (arr[mid] > k) {
-            right = mid - 1;
-            ans = mid;
-        }
-        else left = mid + 1;
+  let left = 0;
+  let right = arr.length - 1;
+  let ans = 0;
+  while (left < right) {
+    const mid = Math.floor((right + left) / 2);
+    if (arr[mid] > k) {
+      right = mid - 1;
+      ans = mid;
+    } else left = mid + 1;
+  }
+  console.log("ans:", ans);
+};
 
+const LowerBoundInLogn = (arr, n, k) => {
+  let i = 0;
+  let j = n - 1;
+  let res = -1;
+  while (i <= j) {
+    let m = Math.floor(i + (j - i) / 2);
+
+    if (arr[m] == k) {
+      res = m;
+      j = m - 1;
+    } else if (arr[m] < k) {
+      i = m + 1;
+    } else {
+      j = m - 1;
     }
-    console.log('ans:', ans)
-}
-binarySearch([1, 3, 5, 7, 9], 5)
+  }
+
+  console.log(res);
+};
+LowerBoundInLogn([1, 1, 2, 2, 5], 5, 2);
+binarySearch([1, 3, 5, 7, 9], 5);
--- Cyberverse.js
+++ Cyberverse.js
@@ -1,92 +1,88 @@
 function runProgram(input) {
-    // Write code here
-    // let arr=input.trim().split("\n");
-    // let cases=arr.slice(1).map(line=>line.split("").map(Number));
-    // let out=cases.map(([N,K])=>{
-    //    sol(N,K)
-    // });
-    // for(let output of out){
-    //     console.log(output);
-    // }
-    /*
+  // Write code here
+  // let arr=input.trim().split("\n");
+  // let cases=arr.slice(1).map(line=>line.split("").map(Number));
+  // let out=cases.map(([N,K])=>{
+  //    sol(N,K)
+  // });
+  // for(let output of out){
+  //     console.log(output);
+  // }
+  /*
    input =  `   1    
             5 8    `
     */
-    input = input.trim().split("\n");
-     /*
+  input = input.trim().split("\n");
+  /*
      trim();
    input =  `1    
             5 8`
     */
-     /*
+  /*
      split();
    input =  ['2','5 8','4 10']
     */
-   const tc = +input[0].trim();
-   let line=1;
-//    line=3
-    for(let i=0;i<tc;i++){
-        // i=1
-        // line=3
-        const [n,k] = input[line].trim().split(" ").map(Number);
-        line++;
-        // line=4;
-        const arr = input[line].trim().split(" ").map(Number);
-        line++
-        fun(n,k,arr);
+  const tc = +input[0].trim();
+  let line = 1;
+  //    line=3
+  for (let i = 0; i < tc; i++) {
+    // i=1
+    // line=3
+    const [n, k] = input[line].trim().split(" ").map(Number);
+    line++;
+    // line=4;
+    const arr = input[line].trim().split(" ").map(Number);
+    line++;
+    fun(n, k, arr);
+  }
+}
+
+function dist(w, c) {
+  let distributed = 0;
+  for (let d = 0; d < c; d++) {
+    distributed = distributed + Math.min(w, d + 1);
+  }
+  return distributed;
+}
+function sol(N, K) {
+  console.log("N,K:", N, K);
+  // N=0;
+  // K =0
+  let min = 0;
+  let max = K;
+  while (max - min > 1) {
+    let mid = Math.floor((max + min) / 2);
+    let distributed = dist(mid, N);
+    if (distributed <= K) {
+      min = mid;
+    } else {
+      max = mid;
     }
+  }
+  return min;
+}
 
-
-  }
-  
-  function dist(w,c){
-      let distributed=0;
-      for(let d=0; d<c; d++){
-          distributed=distributed+Math.min(w,d+1)
-      }
-      return distributed;
-  }
-  function sol(N,K){
-    console.log('N,K:', N,K)
-    // N=0;
-    // K =0
-      let min=0;
-      let max=K;
-      while(max-min>1){
-          let mid=Math.floor((max+min)/2);
-          let distributed=dist(mid,N);
-          if(distributed<=K){
-              min=mid;
-          }
-          else
-          {
-              max=mid;
-          }
-      }
-      return min;
-  }
-  
-  if (process.env.USERNAME=== "ganum") {
-    runProgram(`2
+if (process.env.USERNAME === "ganum") {
+  runProgram(`2
     5 8
     4 10 78 4398 989
     5 10
     4 10 78 43 9`);
-  } else {
-    process.stdin.resume();
-    process.stdin.setEncoding("ascii");
-    let read = "";
-    process.stdin.on("data", function (input) {
-      read += input;
-    });
-    process.stdin.on("end", function () {
-      read = read.replace(/\n$/, "");
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-    });
-    process.on("SIGINT", function () {
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-      process.exit(0) ;
-    });
-  }
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
