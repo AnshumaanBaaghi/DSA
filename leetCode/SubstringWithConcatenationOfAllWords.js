837689a67f3c2bcbb38047ac147762201247726b
anshumaanbaaghi <ganumumbai067@gmail.com>
2024-07-03 13:21:42 +0530

    Substring With Concatenation Of All Words

---
+++ leetCode/SubstringWithConcatenationOfAllWords.js
@@ -0,0 +1,54 @@
+var findSubstring = function (s, words) {
+  const obj = words.reduce((acc, el) => {
+    acc[el] = (acc[el] || 0) + 1;
+    return acc;
+  }, {});
+  const windowSize = words.length * words[0].length;
+  let start = 0;
+  let end = windowSize - 1;
+  const ans = [];
+  while (end < s.length) {
+    const obj2 = { ...obj };
+    let flag = true;
+    for (let i = 0; i < words.length; i++) {
+      const startingIndex = start + words[0].length * i;
+      const endingIndex = start + words[0].length + words[0].length * i;
+      const word = s.substring(startingIndex, endingIndex);
+      if (!obj2[word]) {
+        flag = false;
+        break;
+      }
+      obj2[word] -= 1;
+    }
+    if (flag) {
+      ans.push(start);
+    }
+    start++;
+    end++;
+  }
+  return ans;
+};
+
+const s =
+  "pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel";
+const words = [
+  "dhvf",
+  "sind",
+  "ffsl",
+  "yekr",
+  "zwzq",
+  "kpeo",
+  "cila",
+  "tfty",
+  "modg",
+  "ztjg",
+  "ybty",
+  "heqg",
+  "cpwo",
+  "gdcj",
+  "lnle",
+  "sefg",
+  "vimw",
+  "bxcb",
+];
+console.log(findSubstring(s, words));
